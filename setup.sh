6fa7c25e196826ae227d5a417d15497e9f6c6f26
Pan Dar Py <70007556+Pandar009@users.noreply.github.com>
2021-10-07 04:22:06 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,7 @@ echo "1111 DNS server connecting"
 echo "nameserver 1.1.1.1" > /etc/resolv.conf
 echo "1111 server connect!"
 echo "kali sourse list adding >>>..."
-echo "deb http://http.kali.org/kali kali-experimental main non-free contrib" | sudo tee /etc/apt/sources.list.d/kali-experimental.list
-echo "deb http://http.kali.org/kali kali-last-snapshot main non-free contrib" | sudo tee /etc/apt/sources.list
-echo "deb http://http.kali.org/kali kali-last-snapshot main non-free contrib" | sudo tee /etc/apt/sources.list
+echo "deb http://http.kali.org/kali kali-last-snapshot main non-free contrib" > /etc/apt/sources.list
 echo 'Sourse list adding done!'
 apt update -y & jobs
 apt install dnsutils & jobs
